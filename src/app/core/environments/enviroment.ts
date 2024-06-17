554bdbaa00663700c06e5c280254490c96dad58e
SharonFlorez <shdflorezs35@gmail.com>
2024-06-16 20:27:39 -0500

    Update last chages

--- src/app/core/environments/enviroment.ts
+++ src/app/core/environments/enviroment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyDQRhAwwu6gIhrJDXGg1eiw3TBPj7ZDGIg',
+    authDomain: 'music-player-367ff.firebaseapp.com',
+    projectId: 'music-player-367ff',
+    storageBucket: 'music-player-367ff.appspot.com',
+    messagingSenderId: '412912037740',
+    appId: '1:412912037740:web:30702e2518243502b84d49',
   },
 };
